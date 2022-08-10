e4c2e6bea82a387d9ed0c3f30002c5041233ba01
Natalka1996 <nata-chekavaya@mail.ru>
2022-08-10 10:50:28 +0300

    feat: new index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // write your solution here// преобразуем эту строку в массив по 10 чисел
+    const numberArr = expr.match(/.{10}/g);
+    console.log("numberArr", numberArr);
+    // из каждой десятки получаем точки и тире
+    const symbols = numberArr.map((number10) => {
+        // звездочки значат пробел по условию
+        if (number10 === "**********") { return ' ' }
+    
+    
+        const symbol = number10.match(/.{2}/g).map(n => {
+            // делим по два символа и определяем точка или тире
+            console.log("n", n);
+            if (n === "10") { return "." }
+            if (n === "11") { return "-" }
+            return ""
+        }).join("");
+        // смотрим что получилось
+        console.log("symbol", symbol);
+        return symbol
+    }
+    )
+
+    console.log("symbols", symbols);
+    // на выходе массив из точек и тире преобразуем в буквы и обЪединяем обратно в строку
+    return symbols.map((symbol) => {
+        // пропускаем пробелы
+        if (symbol === " ") { return " " }
+ 
+        return MORSE_TABLE[symbol]
+    }
+    )
+    .join("")
+// Log to console
+console.log("RESULT", decode("00011011110000111111**********0000111111"))
 }
 
 module.exports = {
     decode
+    
 }
